4f7019b1190605ff548497397a8688c783a46ea1
alansteve83@gmail.com <alansteve83@gmail.com>
2020-05-25 10:18:47 -0500

    last changes

--- README.md
+++ README.md
@@ -1,2 +1,3 @@
 # everis_challenge
 1. Modificar archivo creds/serviceaccount.json con el service account con los permisos necesarios.
+2. editar archivo TF con variables de projecto, zona, location (se creará fix con arhcivo *.tfvars)
--- task1.sh
+++ task1.sh
@@ -1,25 +1,25 @@
 #!/bin/bash
 
-#Variables
+# Variables
 export PWD=`pwd`
 export LOCATION='us-central1'
 export ZONE='us-central1-c'
 export PROJECT=$GOOGLE_CLOUD_PROJECT
 
 gcloud config set project $PROJECT
-# Habilitamos Kubernetes Engine API
+# Enable kubernetes engine api
 gcloud services enable container.googleapis.com
 
-#Get/set infor of my environment
+# Get/set infor of my environment
 gcloud config set compute/zone $ZONE
 
-#Terraform - create gke cluster
-#cd $PWD/terraform/
+# Terraform - create gke cluster
+# Crear tfavars file with variables ***
 terraform init
 terraform plan -out create_gke_cluster
 terraform apply "create_gke_cluster" 
-#cd $PWD
 
+# Select the cluster created.
 gcloud container clusters get-credentials challenge-gke-cluster
 
 # nginx ingress controller installation
@@ -30,18 +30,16 @@ git checkout v1.7.0
 kubectl apply -f common/ns-and-sa.yaml
 kubectl apply -f rbac/rbac.yaml
 
-#Crearemos los recursos comunes (por default, estoy agregando todos los que la documentación nginx indica)
+# Crearemos los recursos comunes (por default, estoy agregando todos los que la documentación nginx indica)
 kubectl apply -f common/default-server-secret.yaml
 kubectl apply -f common/nginx-config.yaml
-
 kubectl apply -f deployment/nginx-ingress.yaml
 #kubectl get pods --namespace=nginx-ingress
-
 kubectl apply -f service/loadbalancer.yaml
 #kubectl get svc nginx-ingress --namespace=nginx-ingress
 
 
-#aplicacion - python
+# Aplication - python
 sudo apt-get update
 sudo apt-get install -y -q build-essential python-pip python-dev python-simplejson
 
@@ -51,10 +49,10 @@ docker build -t gcr.io/${PROJECT}/hello-app:v1 .
 gcloud auth configure-docker
 docker push gcr.io/${PROJECT}/hello-app:v1
 
-# kubectl create deployment hello-app --image=gcr.io/${PROJECT}/hello-app:v1
-# kubectl expose deployment hello-app --type=LoadBalancer --port 80 --target-port 8080
+kubectl create deployment hello-app --image=gcr.io/${PROJECT}/hello-app:v1
+kubectl expose deployment hello-app --type=LoadBalancer --port 80 --target-port 8080
 
-#Test
+# Test
 #docker run --rm -p 8080:8080 gcr.io/${PROJECT}/hello-app:v1
 
 #Destroy
